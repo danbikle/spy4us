919b77f858a45abafd236fb49dae42f812e1d532
Dan Bikle <dan.bikle@gmail.com>
2015-04-04 07:44:04 +0000

    enhancements

--- spy4us/spy4us.js
+++ spy4us/spy4us.js
@@ -1,6 +1,6 @@
 if (Meteor.isClient) {
 
-  Template.body.onRendered(function() {
+  Template.hello.onRendered(function() {
     var myh1 = $('pre#pre_ff1');
     var myh2 = myh1.html()     ;
     var myp1 = Papa.parse(myh1);
@@ -9,19 +9,12 @@ if (Meteor.isClient) {
 
   });
 
-  Template.body.helpers({
-    myCollection: function() {
-      return Session.get('ff1csv');
-    }
-  });
-
   // counter starts at 0
   Session.setDefault('counter', 0);
 
   Template.hello.helpers({
-    counter: function () {
-      return Session.get('counter');
-    }
+    myCollection: function() {return Session.get('ff1csv'); }
+    counter:      function() {return Session.get('counter');}
   });
 
   Template.hello.events({
