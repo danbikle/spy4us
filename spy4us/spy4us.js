d2081cf918796c96e59015471f30916d8615fc40
Dan Bikle <dan.bikle@gmail.com>
2015-04-04 06:15:48 +0000

    enhancements

--- spy4us/spy4us.js
+++ spy4us/spy4us.js
@@ -1,10 +1,12 @@
 if (Meteor.isClient) {
 
   Template.body.onRendered(function() {
-    var myh1 = $('pre#pre_ff1').html();
+    var myh1 = $('pre#pre_ff1');
+    var myh2 = myh1.html()     ;
     var myp1 = Papa.parse(myh1);
-    var myp2 = myp1.data;
-    Session.set('ff1csv',myp2);
+    var myp2 = myp1.data       ;
+    Session.set('ff1csv',myp2) ;
+    myh1.hide();
   });
 
   Template.body.helpers({
